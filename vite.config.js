36234d6d44aa10ce240713780b166b171f05c1b8
denglh <su@suncereltd.com>
2023-06-07 11:52:10 +0800

    在public文件夹下托管静态文件

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue()
+  ],
+  resolve: {
+    alias: {
+      // 'cesium': path.resolve(__dirname, cesiumSource)
+    }
+  },
   optimizeDeps: {
     include: ['cesium'],
   },
