588f6db910814b17f6311f68e3d722ce56223953
denglh <su@suncereltd.com>
2023-06-26 18:05:00 +0800

    增加亮度调整功能

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="height:100%">
-    <div class="tools">
-      <v-sheet :width="230" class="pa-4">
+    <div style="width:230px" class="tools">
+      <v-sheet class="pa-4">
         <v-radio-group v-model="store.mode" @update:modelValue="onMode" inline>
           <v-radio label="三维" value="3D"></v-radio>
           <v-radio label="二维" value="2D"></v-radio>
@@ -12,6 +12,29 @@
           return-object @update:modelValue="onChangeLabelLayer"></v-select>
         <v-select label="视角" density :items="geoCode" item-title="name" item-value="coord" variant="outlined"
           return-object @update:modelValue="onCamera"></v-select>
+        <div>
+          <div>colorToAlpha:{{ store.colorToAlpha }}</div>
+          <v-text-field density="compact" v-model="store.colorToAlpha" variant="outlined"></v-text-field>
+          <v-btn flat variant="outlined" @click="colorToAlpha">确定</v-btn>
+          <div>alpha:{{ store.alpha }}</div>
+          <v-slider @update:modelValue="alpha" v-model="store.alpha" min="0" max="1" :step="0.1">
+          </v-slider>
+          <div>brightness:{{ store.brightness }}</div>
+            <v-slider @update:modelValue="brightness" v-model="store.brightness" :max="10" :step="0.1">
+            </v-slider>
+          <div>contrast:{{ store.contrast }}</div>
+          <v-slider @update:modelValue="contrast" v-model="store.contrast" :max="10" :step="0.1">
+          </v-slider>
+          <div>hue:{{ store.hue }}</div>
+          <v-slider @update:modelValue="hue" v-model="store.hue" :max="10" :step="0.1">
+          </v-slider>
+          <div>saturation:{{ store.saturation }}</div>
+          <v-slider @update:modelValue="saturation" v-model="store.saturation" :max="10" :step="0.1">
+          </v-slider>
+          <div>gamma:{{ store.gamma }}</div>
+          <v-slider @update:modelValue="gamma" v-model="store.gamma" :max="10" :step="0.1">
+          </v-slider>
+        </div>
       </v-sheet>
     </div>
     <div id="container"></div>
@@ -23,7 +46,45 @@ import { onMounted, reactive } from "vue";
 import { map, provider, SceneMode } from "@/utils/ceisum.map";
 import { TDT_IMAGE, TDT_Annotation, geoCode } from "@/config/default";
 
-const store = reactive({ items: [], mode: "3D" });
+const store = reactive({ items: [], mode: "3D", alpha:1, colorToAlpha:'#FB8C00', brightness: 0, contrast: 0, hue: 0, saturation: 0, gamma: 0 });
+
+function colorToAlpha() {
+  const imageryLayers = store.viewer.imageryLayers;
+  const layer = imageryLayers.get(0)
+  layer.colorToAlpha = Cesium.Color.BLACK
+  console.log(layer)
+}
+
+function alpha() {
+  const imageryLayers = store.viewer.imageryLayers;
+  const layer = imageryLayers.get(0)
+  layer.colorToAlpha = Cesium.Color.BLACK
+}
+function brightness(val) {
+  const imageryLayers = store.viewer.imageryLayers;
+  const layer = imageryLayers.get(0)
+  layer.alpha = val
+}
+function contrast(val) {
+  const imageryLayers = store.viewer.imageryLayers;
+  const layer = imageryLayers.get(0)
+  layer.contrast = val
+}
+function hue(val) {
+  const imageryLayers = store.viewer.imageryLayers;
+  const layer = imageryLayers.get(0)
+  layer.hue = val
+}
+function saturation(val) {
+  const imageryLayers = store.viewer.imageryLayers;
+  const layer = imageryLayers.get(0)
+  layer.saturation = val
+}
+function gamma(val) {
+  const imageryLayers = store.viewer.imageryLayers;
+  const layer = imageryLayers.get(0)
+  layer.gamma = val
+}
 function onLoadingLayer() {
   console.log(store)
 }
@@ -58,6 +119,17 @@ function onCamera(obj) {
 onMounted(() => {
 
   store.viewer = map("container");
+
+  const imageryLayers = store.viewer.imageryLayers;
+
+  const layer = imageryLayers.get(0)
+  store.brightness = layer.brightness
+  store.contrast = layer.contrast
+  store.hue = layer.hue
+  store.saturation = layer.saturation
+  store.gamma = layer.gamma
+
+  console.log(layer)
 
 
   // const promise = Cesium.GeoJsonDataSource.load("/json/中华人民共和国.json")
--- src/main.js
+++ src/main.js
@@ -6,13 +6,29 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+const myCustomLightTheme = {
+  dark: false,
+  colors: {
+    background: '#6200EE',
+    surface: '#018786',
+    primary: '#6200EE',
+    'primary-darken-1': '#3700B3',
+    secondary: '#03DAC6',
+    'secondary-darken-1': '#018786',
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00',
+  },
+}
 const vuetify = createVuetify({
   components,
   directives,
-  // defaults: {
-  //   VSheet: {
-  //     style: [{ border: '2px solid #ce836a', backgroundColor:'#0dbc79'}]
-  //   }
-  // }
+  defaults: {
+    // VSheet: {
+    //   style: [{ coloo: '#ffffff', backgroundColor:'rgba(42, 42, 42, 0.8)'}]
+    // },
+    // vSelect: [{ color: '#ffffff', border: '2px solid #ffffff', backgroundColor:'#ffffff'}]
+  },
 })
 createApp(App).use(vuetify).mount('#app')
