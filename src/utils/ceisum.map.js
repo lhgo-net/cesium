bce7ceeff7271a0c1281b62a0f8d2abf165f19d0
denglh <su@suncereltd.com>
2023-08-03 17:40:38 +0800

    影像加载位置调整

--- src/utils/ceisum.map.js
+++ src/utils/ceisum.map.js
@@ -56,7 +56,6 @@ export function SceneMode(viewer, val) {
   }
 }
 
-
 export function highlighCity(geo) {
   const globe = viewer.scene.globe
   globe.translucency.enabled = true
@@ -74,21 +73,18 @@ export function highlighCity(geo) {
   viewer.terrainProvider = new Cesium.CesiumTerrainProvider({
     url: '/GISFile/terrain/xn'
   })
+  viewer.imageryLayers.addImageryProvider(new Cesium.UrlTemplateImageryProvider({
+    url: '/GISFile/imageLayer/xn/{z}/{x}/{y}.png'
+  }))
   for (let i = 0;i < geoJSON.length;i++) {
     pointList.push(Cesium.Cartographic.fromDegrees(geoJSON[i][0], geoJSON[i][1]))
   }
   const promise = Cesium.sampleTerrainMostDetailed(viewer.terrainProvider, pointList)
   promise.then(updatedPositions => {
-    console.log(viewer)
-
     for (let i = 0;i < updatedPositions.length;i++) {
       maxHeight.push(updatedPositions[i].height)
     }
-    viewer.imageryLayers.addImageryProvider(new Cesium.UrlTemplateImageryProvider({
-      url: '/GISFile/imageLayer/xn/{z}/{x}/{y}.png'
-    }))
-
-    viewer.entities.add({
+    const wall = viewer.entities.add({
       id: 'wall',
       wall: {
         positions: Cesium.Cartesian3.fromDegreesArray(coordinates),
@@ -97,7 +93,6 @@ export function highlighCity(geo) {
         material: getColorRamp('rgb(52,192,214)', 'rgb(15,93,180)')
       }
     })
-    viewer.zoomTo(viewer.entities)
-
+    viewer.zoomTo(wall)
   })
 }
